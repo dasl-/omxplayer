e745fdc329cf6879e45310c2ac50b25cdff22280
dasl- <david.leibovic@gmail.com>
2021-07-23 03:14:35 -0400

    Decrease default reading size for ffmpeg to speed up reads in the main loop

--- OMXReader.h
+++ OMXReader.h
@@ -44,7 +44,7 @@ using namespace std;
 #define MAX_OMX_STREAMS        100
 
 #ifndef FFMPEG_FILE_BUFFER_SIZE
-#define FFMPEG_FILE_BUFFER_SIZE   32768 // default reading size for ffmpeg
+#define FFMPEG_FILE_BUFFER_SIZE   4096 // default reading size for ffmpeg
 #endif
 #ifndef MAX_STREAMS
 #define MAX_STREAMS 100
